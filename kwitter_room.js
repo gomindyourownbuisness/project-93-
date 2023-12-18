9d93ad1ba9c0d5bb103628ef668ac71e2e86420b
gomindyourownbuisness <130389857+gomindyourownbuisness@users.noreply.github.com>
2023-12-18 23:50:40 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,55 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+      apiKey: "AIzaSyCYeGRGJK0M6D2rJGIT6hLgGrI3cyohts4",
+      authDomain: "kwitter-b0864.firebaseapp.com",
+      databaseURL: "https://kwitter-b0864-default-rtdb.firebaseio.com",
+      projectId: "kwitter-b0864",
+      storageBucket: "kwitter-b0864.appspot.com",
+      messagingSenderId: "107157461214",
+      appId: "1:107157461214:web:f825ebd9499a0cfdddca4f"
+    };
+    
+    // Initialize Firebase
+     firebase.initializeApp(firebaseConfig);
+     
+var user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "welcome " + user_name + "!"
+
+function addRoom()
+{
+ var room_name = document.getElementById("room_name").value ; 
+ firebase.database().ref("/").child(room_name).update({
+      purpose:"adding room name"
+ });
+ localStorage.setItem("room_name" , "room_name");
+ window.location="kwitter_page.html";
+}
+function getData() {
+        firebase.database().ref("/").on('value', function(snapshot) 
+        {document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function(childSnapshot) {
+            childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      console.log("Room Name - " + Room_names);
+      row="<div class='room_name' id=" + Room_names + "onclick = 'redirectToRoomName(this.id)'>#" + Room_names + "</div> <hr> ";
+      document.getElementById("output").innerHTML +=row;
+      //End code
+      });});}
+getData();
+
+function redirectToRoomName(name)
+{
+     console.log(name);
+     localStorage.setItem("room_name", name);
+     window.location = "kwitter_page.html";
+
+}
+
+function logout () 
+{
+     localStorage.removeItem("user_name");
+     localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
